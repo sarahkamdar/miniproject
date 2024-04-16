626c66702ad2e4f50a7ffc0ba11fce633afcee39
sarahkamdar <sarahkamdar90@gmail.com>
2024-04-16 23:14:38 +0530

    done

--- credit_card_validator.cpp
+++ credit_card_validator.cpp
@@ -1,1 +1,44 @@
- #include<
+ #include<iostream>
+ using namespace std;
+
+ int getdigit(const int no);
+ int sumOddDigits(const string CreditCardNo);
+ int sumEvenDigits(const string CreditCardNo);
+
+int main(){
+
+    string CreditCardNo;
+    int result;
+    cout << "Enter your credit card number : ";
+    cin >> CreditCardNo;
+
+    result =  sumOddDigits(CreditCardNo) + sumEvenDigits(CreditCardNo);
+
+    if(result % 10 == 0){
+        cout << CreditCardNo << " is valid" << endl;
+    }
+    else{
+        cout << CreditCardNo << " is not valid" << endl;
+    }
+ return 0;
+}
+
+int getdigit(const int no){
+    return no % 10 + (no / 10) % 10;
+}
+
+int sumOddDigits(const string CreditCardNo){
+    int sum = 0;
+    for(int i = CreditCardNo.size()-1; i >= 0; i-=2){
+        sum += getdigit(CreditCardNo[i] - '0');
+    }
+    return sum;
+}
+
+int sumEvenDigits(const string CreditCardNo){
+int sum = 0;
+    for(int i = CreditCardNo.size()-2; i >= 0; i-=2){
+        sum += getdigit((CreditCardNo[i] - '0')*2);
+    }
+    return sum;
+}
